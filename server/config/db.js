5b89a8f72aef236c91e447122e4f1ec620db1ed6
Ece Irem Burgaz <eburgaz@my.centennialcollege.ca>
2021-10-27 18:26:38 -0400

    COMP229-F2021-MidTerm-301162609

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://eburgaz:mSMZ9oYsnxuXzM5n@mongodbserver.jfoha.mongodb.net/booksData?retryWrites=true&w=majority"
+};
